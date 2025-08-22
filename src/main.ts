001fa80c816546e640a61185d239c152b1e038be
YC <30573421+YalimTech@users.noreply.github.com>
2025-08-22 18:07:22 -0400

    chore: replace logo with svg

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,8 @@ async function bootstrap() {
 
   // === Servir archivos estáticos ===
   // Sirve {projectRoot}/public en la RAÍZ del dominio.
-  // Ej.: public/LogoIcon.png -> https://tu-dominio/LogoIcon.png
-  app.useStaticAssets(join(__dirname, '..', 'public'));
+  // Ej.: public/LogoIcon.svg -> https://tu-dominio/LogoIcon.svg
+  app.useStaticAssets(join(process.cwd(), 'public'));
 
   // === Configuración existente ===
   app.use(json({ limit: '1mb' }));
